889a7c2132f95519a2e04d41f633132811447b1f
Vin22-03 <vinay.venvin@gmail.com>
2025-11-04 14:32:48 +0000

    Updated frontend and backend

--- app/frontend/app/farm/[id]/page.tsx
+++ app/frontend/app/farm/[id]/page.tsx
@@ -71,7 +71,7 @@ export default function FarmDetailsPage() {
     setAiResult("");
 
     try {
-      const res = await fetch("http://localhost:8090/api/v1/predict", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
--- app/frontend/app/predict/page.tsx
+++ app/frontend/app/predict/page.tsx
@@ -29,7 +29,7 @@ export default function PredictPage() {
     setTimestamp("");
 
     try {
-      const res = await fetch("http://localhost:8090/api/v1/predict", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
